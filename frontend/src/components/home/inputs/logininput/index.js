888b31ce10fb3a773002fdeba1ef04811b06cef0
Tanner Oaks <tjoaks@protonmail.com>
2022-09-25 03:37:31 -0400

    updated 9/25

---
+++ frontend/src/components/home/inputs/logininput/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+export default function LoginInput(options) {
+  return <div>LoginInput</div>;
+}
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,3 +1,46 @@
+import './style.css';
+import { Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import LoginInput from '../../components/home/inputs/logininput';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../icons/facebook.svg" alt=""></img>
+            <span>
+              Facebook recreation project by Tanner Oaks. Created with Node.js
+              and React.
+            </span>
+            <div className="login_2">
+              <div className="login_2_wrap">
+                <Formik>
+                  {(formik) => (
+                    <Form>
+                      <LoginInput />
+                      <button type="submit" className="blue_btn">
+                        Log in
+                      </button>
+                    </Form>
+                  )}
+                </Formik>
+                <Link to="/forgot" className="forgot_password">
+                  Forgot your password?
+                </Link>
+                <div className="sign_splitter"></div>
+                <button className="blue_btn open_signup">Create Account</button>
+              </div>
+              <Link to="/" className="sign_extra">
+                <b>Create A Page </b>
+                for a celebrity, brand, or business.
+              </Link>
+            </div>
+          </div>
+          <div className="register"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
